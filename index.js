5ddf59f628cc97f35dab6edd73c421caa0724ede
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-01-06 00:34:17 +0600

    admin api handled

--- index.js
+++ index.js
@@ -32,10 +32,48 @@ async function run() {
     const reviewCollection = client.db("bistroBossDB").collection("reviews");
     const cartCollection = client.db("bistroBossDB").collection("carts");
 
+    // jwt related api
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res.send({token});
+    })
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log(req.headers.authorization);
+
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: 'forbidden acccess'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
     // users related api
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyToken,  async(req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
+    })
+
+    app.get('/users/admin/:email', verifyToken, async(req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+        res.send({admin});
+      }
     })
 
     app.post('/users', async(req, res) => {
